c7ffcc01dea04de996e343bec33b8de64c0939d4
Lim, seokhyeon <selver44444@gmail.com>
2021-09-28 09:45:13 +0900

    201732029-4

--- src/git_exam1/hello.java
+++ src/git_exam1/hello.java
@@ -3,8 +3,8 @@ package git_exam1;
 public class hello {
 	
 	public static void main(String []args) {
-		System.out.println("IT융합자율학부");
-		System.out.println("201732029");
-		System.out.println("임석현");
+		System.out.println("소속: IT융합자율학부");
+		System.out.println("학번: 201732029");
+		System.out.println("이름: 임석현");
 	}
 }
